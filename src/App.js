3bd6543b94fd9e641f8168e858f82ae4921148b0
qiacheng <choliaky@gmail.com>
2020-05-07 14:01:44 -0700

    implmeneted a prototype for g Auth

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+import GoogleLogin from 'react-google-login'
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      SignedIn : false,
+      username : '',
+      email: '',
+      profileimg: '',
+    }
+  }
+
+  responseGoogle = (response) => {
+    console.log(response);
+    this.setState(
+      {
+        username : response.profileObj.name,
+        email: response.profileObj.email,
+        profileimg: response.profileObj.imageUrl,
+      }
+    )
+    console.log(this.state)
+  }
+
+  
+  render(){
+    return (
+      <div className="App">
+        <p id="Name">Name: {this.state.username}</p>
+        <p id="Email">Email: {this.state.email}</p>
+        <br/>
+        <img id="profileimg" src={this.state.profileimg}></img>
+        <br/>
+        <br/>
+
+        {this.G_login()}
+        
+      </div>
+    );
+  }
+
+  G_login(){
+    if(this.state.SignedIn){
+      return(
+      <div><p>Already Signed In</p></div>
+      )
+    }
+    else{
+      return(
+        <GoogleLogin
+          clientId ="985223166095-8i4k79t4c9uqlq5sbe1sljmvq1cefn5r.apps.googleusercontent.com"
+          buttonText = "Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        ></GoogleLogin>
+        
+      )
+    }
+
+  }
 }
-
-export default App;
