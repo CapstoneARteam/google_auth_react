e9927cf5f8d8f2773d7f62403fd333d02196dfcb
qiacheng <choliaky@gmail.com>
2020-05-19 04:06:23 -0700

    tmp

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import GoogleLogin from 'react-google-login'
 //import logo from './logo.svg';
 //import './App.css';
 
-import {Stitch, AnonymousCredential, RemoteMongoClient} from "mongodb-stitch-browser-sdk"
+import {Stitch, AnonymousCredential, RemoteMongoClient, GoogleRedirectCredential} from "mongodb-stitch-browser-sdk"
 
 
 export default class App extends Component{
   constructor(){
     super()
     this.state = {
-      SignedIn : false,
       username : '',
       email: '',
+      userId:'',
       profileimg: '',
       email_test:'',
       name_test:'',
+      account_type_test: 'user',
       stitch_res:[],
     }
     this.addUser = this.addUser.bind(this);
@@ -25,7 +26,8 @@ export default class App extends Component{
     this.namechange = this.namechange.bind(this);
     this.SearchUser=this.SearchUser.bind(this);
     this.ChangeEmail=this.ChangeEmail.bind(this);
-
+    this.acctypechange=this.acctypechange.bind(this);
+    this.login = this.login.bind(this);
     this.OldEmail = React.createRef();
     this.NewEmail = React.createRef();
 
@@ -37,10 +39,55 @@ export default class App extends Component{
       RemoteMongoClient.factory,
       "mongodb-atlas"
     );
-    this.db = mongodb.db("ACCOUNTS");
-    this.client.auth.loginWithCredential(new AnonymousCredential()).then(
-      console.log(this.db.collection("USERS").find({}, {limit: 50}).asArray())
-    ).catch(console.error)
+    this.db = mongodb.db("APP");
+    
+
+  }
+
+
+  async login() {
+    //copy the name of your google-auth enabled stitch application here
+    //the name of the app will typically be the stitch application name
+    //with a "-"" + random string appended
+    
+  
+    // Get a client for your Stitch app, or instantiate a new one
+  
+    //manage user authentication state
+    
+    // Check if this user has already authenticated and we're here
+    // from the redirect. If so, process the redirect to finish login.
+    if (this.client.auth.hasRedirectResult()) {
+      await this.client.auth.handleRedirectResult().then(
+        this.setState(
+          {
+            username : this.client.auth.authInfo.userProfile.data.first_name,
+            email: this.client.auth.authInfo.userProfile.data.email,
+            userId: this.client.auth.authInfo.userID,
+          }
+        )
+      );
+      console.log("Processed redirect result.")
+    }
+  
+    if (this.client.auth.isLoggedIn) {
+      // The user is logged in. Add their user object to component state.
+      const currentUser = this.client.auth.user;
+      this.setState({ currentUser });
+      
+      this.setState(
+        {
+          username : this.client.auth.authInfo.userProfile.data.first_name,
+          email: this.client.auth.authInfo.userProfile.data.email,
+          userId: this.client.auth.authInfo.userId,
+        }
+      )
+      console.log(this.state)
+    } else {
+      // The user has not yet authenticated. Begin the Google login flow.
+      const credential = new GoogleRedirectCredential();
+      this.client.auth.loginWithRedirect(credential);
+    }
   }
 
   responseGoogle = (response) => {
@@ -56,17 +103,19 @@ export default class App extends Component{
     this.db.collection("USERS")
       .insertOne({
         email: this.state.email,
-        name: this.state.username 
+        name: this.state.username,
+        account_type: this.state.account_type_test
       })
       .catch(console.error);
   }
 
-  addUser(event) {
-    event.preventDefault()
+  addUser() {
     this.db.collection("USERS")
       .insertOne({
+        userID: this.state.userId,
         email: this.state.email_test,
-        name: this.state.name_test 
+        name: this.state.name_test,
+        account_type: this.state.account_type_test,
       })
       .catch(console.error);
   }
@@ -75,8 +124,11 @@ export default class App extends Component{
   getallusers() {
     this.db.collection("USERS").find({})
     .asArray()
-    .then(stitch_res => this.setState({stitch_res}))
-    console.log(this.state.stitch_res)
+    .then((stitch_res) => {this.setState({stitch_res})
+      console.log(this.state.stitch_res[0])
+    }
+    )
+    
   }
   
   emailchange(event){
@@ -85,6 +137,10 @@ export default class App extends Component{
   namechange(event){
     this.setState({name_test: event.target.value})
   }
+  acctypechange(event){
+    this.setState({account_type_test: event.target.value})
+  }
+
   SearchUser(event)
   {
     const query ={"email":event.target.value};
@@ -126,18 +182,22 @@ export default class App extends Component{
         <br/>
 
         {this.G_login()}
-        
+        <button onClick={this.login}>abcd test</button>
         <br />
         <br />
         <input type="text" id ="nameinput" placeholder="Enter name here" onChange={this.namechange}/>
         <input type="text" id ="emailinput" placeholder="Enter email here" onChange={this.emailchange}/>
+        <select id="account type" onChange={this.acctypechange}>
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
         <button id="add" onClick={this.addUser}>add </button>
         <br />
 
         <button id="list" onClick={this.getallusers}>list all</button>
         <ul>
-          {this.state.stitch_res.map(info => {
-            return <li>{info.email}  {info.name}</li>
+          {this.state.stitch_res.map((info,idx) => {
+            return <li key={idx}>{info.email} / {info.name} / {info.account_type}</li>
           })}
         </ul>
         <input type="text" id="search" placeholder="Enter email to search"onClick={this.SearchUser}/>
